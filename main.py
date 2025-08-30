3709d953a2e6412228cf6586f506b3cb8940c613
chouhan800 <jadu63334@gmail.com>
2025-08-30 17:49:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -67,7 +67,7 @@ api_url = "http://master-api-v3.vercel.app/"
 api_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNzkxOTMzNDE5NSIsInRnX3VzZXJuYW1lIjoi4p61IFtvZmZsaW5lXSIsImlhdCI6MTczODY5MjA3N30.SXzZ1MZcvMp5sGESj0hBKSghhxJ3k1GTWoBUbivUe1I"
 token_cp ='eyJjb3Vyc2VJZCI6IjQ1NjY4NyIsInR1dG9ySWQiOm51bGwsIm9yZ0lkIjo0ODA2MTksImNhdGVnb3J5SWQiOm51bGx9r'
 adda_token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJkcGthNTQ3MEBnbWFpbC5jb20iLCJhdWQiOiIxNzg2OTYwNSIsImlhdCI6MTc0NDk0NDQ2NCwiaXNzIjoiYWRkYTI0Ny5jb20iLCJuYW1lIjoiZHBrYSIsImVtYWlsIjoiZHBrYTU0NzBAZ21haWwuY29tIiwicGhvbmUiOiI3MzUyNDA0MTc2IiwidXNlcklkIjoiYWRkYS52MS41NzMyNmRmODVkZDkxZDRiNDkxN2FiZDExN2IwN2ZjOCIsImxvZ2luQXBpVmVyc2lvbiI6MX0.0QOuYFMkCEdVmwMVIPeETa6Kxr70zEslWOIAfC_ylhbku76nDcaBoNVvqN4HivWNwlyT0jkUKjWxZ8AbdorMLg"
-photologo = '' #https://envs.sh/GV0.jpg
+photologo = 'https://tinypic.host/images/2025/08/30/IMG_20250727_093946_936.th.jpg' #https://envs.sh/GV0.jpg
 photoyt = 'https://tinypic.host/images/2025/03/18/YouTube-Logo.wine.png' #https://envs.sh/GVi.jpg
 photocp = 'https://tinypic.host/images/2025/03/28/IMG_20250328_133126.jpg'
 photozip = 'https://envs.sh/cD_.jpg'
@@ -587,7 +587,7 @@ async def txt_handler(bot: Client, m: Message):
     "**┃ 🔻**  `480`  \n"
     "**┃ 🔻**  `720`  \n"
     "**┃ 🔻** `1080`  \n"
-    "**╰━━━⌈ 🤖 By 🂾 ⌯mr.chouhan ⌋━━━╯**"
+    "**╰━━━⌈ 🤖 By 🂾 ⌯mr.chouhan🦅 ⌋━━━╯**"
 )
 
     input2: Message = await bot.listen(editable.chat.id)
@@ -924,7 +924,7 @@ async def txt_handler(bot: Client, m: Message):
                            f"┃ 🖇️ **ᴀᴘɪ ᴜʀʟ**: <a href='{url}'>ᴀᴘɪ ʟɪɴᴋ</a>\n" \
                            f"━━━━━━━━━━━━━━━━━━━━━━━\n" \
                            f"🛑 **sᴇɴᴅ** /stop **ᴛᴏ ᴄᴀɴᴄᴇʟ**\n\n" \
-                           f"╰━━━━━ ✦ 𝐵𝑜𝓉 𝓂𝒶𝒹𝑒 𝒷𝓎 mr.chouhan ✦ "
+                           f"╰━━━━━ ✦ 𝐵𝑜𝓉 𝓂𝒶𝒹𝑒 𝒷𝓎 MR.𝐂𝐡𝐨𝐮𝐡𝐚𝐧⚔️✨🦋🦅 ✦ "
 
                     prog = await m.reply_text(Show, disable_web_page_preview=True)
                     try:
@@ -970,7 +970,7 @@ async def txt_handler(bot: Client, m: Message):
                            f"┃ 🖇️ **ᴀᴘɪ ᴜʀʟ**: <a href='{url}'>ᴀᴘɪ ʟɪɴᴋ</a>\n" \
                            f"━━━━━━━━━━━━━━━━━━━━━━━\n" \
                            f"🛑 **sᴇɴᴅ** /stop **ᴛᴏ ᴄᴀɴᴄᴇʟ**\n\n" \
-                           f"**╰━━━━━ ✦ 𝐵𝑜𝓉 𝓂𝒶𝒹𝑒 𝒷𝓎 mr.chouhan ✦ **"
+                           f"**╰━━━━━ ✦ 𝐵𝑜𝓉 𝓂𝒶𝒹𝑒 𝒷𝓎 MR.𝐂𝐡𝐨𝐮𝐡𝐚𝐧⚔️✨🦋🦅 ✦ **"
 
                     prog = await m.reply_text(Show, disable_web_page_preview=True)
                     try:
@@ -1016,7 +1016,7 @@ async def txt_handler(bot: Client, m: Message):
                            f"┃ 🖇️ **ᴀᴘɪ ᴜʀʟ**: <a href='{url}'>ᴀᴘɪ ʟɪɴᴋ</a>\n" \
                            f"━━━━━━━━━━━━━━━━━━━━━━━\n" \
                            f"🛑 **sᴇɴᴅ** /stop **ᴛᴏ ᴄᴀɴᴄᴇʟ**\n\n" \
-                           f"╰━━━━━ ✦ 𝐵𝑜𝓉 𝓂𝒶𝒹𝑒 𝒷𝓎 mr.chouhan✦"
+                           f"╰━━━━━ ✦ 𝐵𝑜𝓉 𝓂𝒶𝒹𝑒 𝒷𝓎 MR.𝐂𝐡𝐨𝐮𝐡𝐚𝐧⚔️✨🦋🦅✦"
                     
                     prog = await m.reply_text(Show, disable_web_page_preview=True)
                     try:
